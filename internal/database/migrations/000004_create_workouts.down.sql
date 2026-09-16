54e1cb95e36e9a497e29d815b73efedf725efbe0
Robin Dittmar <robindittmar@gmail.com>
2026-09-16 12:23:34 +0200

    fix: drop table bodyweight_logs on down migration

--- internal/database/migrations/000004_create_workouts.down.sql
+++ internal/database/migrations/000004_create_workouts.down.sql
@@ -1,5 +1,6 @@
 DROP TABLE IF EXISTS workout_sets;
 DROP TABLE IF EXISTS workouts;
+DROP TABLE IF EXISTS bodyweight_logs;
 DROP TABLE IF EXISTS template_exercises;
 DROP TABLE IF EXISTS templates;
 DROP TABLE IF EXISTS exercises;
